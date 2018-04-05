0cb34e1002f29f6b6e6e94fde1d3d3de2ff57bfc
Thais Cailet <thais.cailet@gmail.com>
2018-04-05 08:29:48 -0500

    Closes #1 Load environment variables

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('Export twitter keys');
+exports.twitter ={
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+console.log('Export spotify keys');
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
