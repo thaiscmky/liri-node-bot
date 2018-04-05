5fd9976689a78aeec3f0cbdf7aa7ea7962c7a537
Thais Cailet <thais.cailet@gmail.com>
2018-04-05 12:20:16 -0500

    #9 Import required packages

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
 var env = require("dotenv");
 //initiate environment variables
 env.config();
+
+//load required packages
+var fs = require("fs");
+var request = require("request");
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
