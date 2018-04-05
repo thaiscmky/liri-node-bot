1663631b747403749e5293d921d91c634470271e
Thais Cailet <thais.cailet@gmail.com>
2018-04-05 15:44:05 -0500

    Closes #12 Setup function for movie-this

--- liri.js
+++ liri.js
@@ -18,7 +18,7 @@ function testCommands(){
     console.log("Running initial tests");
 
     //User input should have at least one argument
-    console.assert(userInputArr.length > 0, `You forgot to enter a command. Here are your options:\n
+    console.assert(userInputArr.length < 1, `You forgot to enter a command. Here are your options:\n
     - liri.js my-tweets //shows last 20 tweets\n
     - liri.js movie-this "Movie you want to search for" //gets information about movie\n
     - liri.js spotify-this-song "Song you want to search for" //gets information about the song\n
@@ -26,10 +26,10 @@ function testCommands(){
     //User input should have no more than 2 arguments
     console.assert(userInputArr.length < 3, `Too many arguments; are you entering 'node liri.js [what-to-do] "what to search"'?`);
     //User asks to search for movie "Black Panther"
-    console.log('The following should return an object with information on movie Black Panther. It returns:');
+    console.log('The following should return an array with information on movie Black Panther. It returns:');
     console.log(issueCommand(['movie-this', 'Black Panther']));
     //User asks to search for song "Money Trees" on spotify
-    console.log('The following should return an object with information on song "Money Trees". It returns:');
+    console.log('The following should return an array with information on song "Money Trees". It returns:');
     console.log(issueCommand(['spotify-this-song', 'Money Trees']));
     //User asks to run chores.txt
     console.log('The following should return at least the results for a search in Spotify for "Hakuna Matata". It returns:');
@@ -49,12 +49,42 @@ function issueCommand(args){
     });
     var fx = args.length > 1 ? `${args[0]}("${args[1]}")` : `${args[0]}()`;
 
-    if(typeof window[args[0]] !== "function")
+    if(typeof args[0] !== "function")
         return `That's not a valid command. Here are your options:\n
     - liri.js my-tweets //shows last 20 tweets\n
     - liri.js movie-this "Movie you want to search for" //gets information about movie\n
     - liri.js spotify-this-song "Song you want to search for" //gets information about the song\n
     - liri.js do-what-it-says //runs a list of commands based on chores.txt\n`;
 
-    eval(fx);
+    responseArray = eval(fx);
+    responseArray.forEach(function(item){
+       console.log(`${item}\n`);
+    });
+}
+
+function movieThis(movie) {
+    //Setup a default in case of null
+    if(typeof movie === null)
+        movie = "Mr. Nobody";
+
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&imdbRating=&tomatoRating=&country=&language=&plot=short&actors=&apikey=trilogy";
+
+    request(queryURL, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var dataObj = JSON.parse(body);
+            return [
+                `Title: ${dataObj.Title}`,
+                `Year: ${dataObj.Year}`,
+                `IMDB Rating: ${dataObj.imdbRating}`,
+                `Rotten Tomatoes Rating: ${dataObj.Ratings[1].Value}`,
+                `Country: ${dataObj.Country}`,
+                `Language: ${dataObj.Language}`,
+                `Actors: ${dataObj.Actors}`,
+                `Plot: ${dataObj.Plot}`
+            ];
+        } else {
+            console.log('Something went wrong and we could not retrieve the information from the server...');
+            if (error) throw error;
+        }
+    });
 }
